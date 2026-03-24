22916373ecf0414893ae8b0dc17ca1dca717a7ea
maria.rodrigues.dbt@gmail.com <maria.rodrigues.dbt@gmail.com>
2026-03-24 13:03:10 +0000

    ok

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -4,7 +4,7 @@
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
 
-    Try changing "table" to "view" below test CI job
+    Try changing "table" to "view" below test CI job promotion
 */
 
 {{ config(materialized='table') }}
