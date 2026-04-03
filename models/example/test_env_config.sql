c9d3231e5c0fa28eba6e1d2313edad7730beaacb
maria rodrigues <maria.rodrigues.dbt@gmail.com>
2026-04-03 05:21:50 +0000

    ok

--- models/example/test_env_config.sql
+++ models/example/test_env_config.sql
@@ -7,7 +7,7 @@
 /* 
    This model doesn't need any source tables. 
    It just returns the values of your environment variables 
-   so you can verify they are working.
+   so you can verify they are workingdd.
 */
 
 select
