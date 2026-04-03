4ca2705007319a7c3b3b262ad1dd0d51568316c2
maria rodrigues <maria.rodrigues.dbt@gmail.com>
2026-04-03 05:14:18 +0000

    ok

---
+++ models/example/test_env_config.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='table',
+    database=env_var('DBT_DATABASENAME', target.database),
+    schema=env_var('DBT_SCHEMANAME', 'default_testing_schema')
+) }}
+
+/* 
+   This model doesn't need any source tables. 
+   It just returns the values of your environment variables 
+   so you can verify they are working.
+*/
+
+select
+    '{{ env_var("DBT_DATABASENAME", "NOT_FOUND") }}' as env_var_database,
+    '{{ env_var("DBT_SCHEMANAME", "NOT_FOUND") }}' as env_var_schema,
+    current_timestamp() as run_at
